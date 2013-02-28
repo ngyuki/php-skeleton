4f1253f7bde6f38a9747db6ddf0642f9e26f89e3
tsyk goto <ngyuki.ts@gmail.com>
2013-02-28 09:05:07 +0900

    enable PHPUnit/Framework/Assert/Functions.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,6 @@
 <?php
+require_once 'PHPUnit/Framework/Assert/Functions.php';
+
 call_user_func(function () {
     $loader = require __DIR__ . '/../vendor/autoload.php';
     $loader->add('ngyuki\Tests', __DIR__);
