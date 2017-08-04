0b3dafba25e76707390b8403ffdbd360f609cdfb
ngyuki <ngyuki.jp@gmail.com>
2017-08-04 15:25:15 +0900

    Fix doc comment

--- src/Sample.php
+++ src/Sample.php
@@ -1,19 +1,6 @@
 <?php
-/**
- * Skeleton Sample
- *
- * @package   ngyuki\Skeleton
- * @copyright 2012 ngyuki <ngyuki.ts@gmail.com>
- * @author    ngyuki <ngyuki.ts@gmail.com>
- * @license   http://www.opensource.org/licenses/mit-license.php MIT License
- * @link      https://github.com/ngyuki/php-skeleton
- */
-
 namespace ngyuki\Skeleton;
 
-/**
- * Skeleton Sample
- */
 class Sample
 {
     public static function main($argc, $argv)
